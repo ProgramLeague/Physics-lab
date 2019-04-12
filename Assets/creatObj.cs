d359ca9ebca8e5a92d0b00e9a1c14c4d415784c5
PC_201701151103\Administrator <807334102@qq.com>
2019-04-13 02:20:34 +0800

    对心碰撞

--- Assets/creatObj.cs
+++ Assets/creatObj.cs
@@ -2,13 +2,18 @@
 
 public class creatObj : MonoBehaviour {
 
-    public static GameObject creatObject(Vector3 v, float m = 1, float q = 0, float size = 0)
+    public static GameObject creatObject(Vector3 v, float m = 1, float q = 0, float e = 1, float size = 0)
     {
         GameObject obj = unityFire.addItem("Sphere", v);
         unityFire.setItemScale(obj, size, size, size);
         phyObj po = obj.GetComponent<phyObj>();
         po.setm(m);
         po.setq(q);
+        if (e > 1 || e < 0)
+        {
+            //碰撞系数在0-1之间，应判断一下
+        }
+        po.sete(e);
         return obj;
     }
 
--- Assets/phyObj.cs
+++ Assets/phyObj.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class phyObj : MonoBehaviour {
 
-    float q=0;
+    float q = 0;//电荷量
+    float e = 1;//碰撞因数（碰撞系数）
 
     public float getq() { return this.q; }
-    public void setq(float q) { this.q = q; }
+    public float gete() { return this.e; }
 
+    public void setq(float q) { this.q = q; }
+    public void sete(float e) { this.e = e; }
+    
+    
 	public float getm()
     {
         Rigidbody rb = this.gameObject.GetComponent<Rigidbody>();
@@ -29,6 +34,23 @@ public class phyObj : MonoBehaviour {
         this.eF = new electrostaticIField(this.gameObject);
         init.allField.Add(this.ugF);
         init.allField.Add(this.eF);
+    }
+
+    private void OnCollisionEnter(Collision collision)//只针对了对心碰撞？
+    {
+        var gameobj1 = this.gameObject;
+        var phyobj1 = GetComponent<phyObj>();
+        var gameobj2 = collision.gameObject;
+        var phyobj2 = GetComponent<phyObj>();
+        var m1 = phyobj1.getm();
+        var v1 = unityFire.getItemVelocity(gameobj1);
+        var m2 = phyobj2.getm();
+        var v2 = unityFire.getItemVelocity(gameobj2);
+        var e = phyobj1.gete() > phyobj2.gete() ? phyobj1.gete() : phyobj2.gete();
+        var newv1 = (m1 * v1 + m2 * v2 - e * m2 * (v1 - v2)) / (m1 + m2);
+        var newv2 = (m1 * v1 + m2 * v2 - e * m1 * (v2 - v1)) / (m1 + m2);
+        unityFire.setItemVelocity(gameobj1, 1, newv1);
+        unityFire.setItemVelocity(gameobj2, 1, newv2);
     }
 
     private void Update()
