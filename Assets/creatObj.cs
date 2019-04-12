8bd853900895c3a5a790420b496f95e1f9a9b13f
3A <524019351@qq.com>
2019-04-12 23:23:14 +0800

    支持指定obj尺寸

--- Assets/creatObj.cs
+++ Assets/creatObj.cs
@@ -2,9 +2,10 @@
 
 public class creatObj : MonoBehaviour {
 
-    public static GameObject creatObject(Vector3 v, float m = 1, float q = 0)
+    public static GameObject creatObject(Vector3 v, float m = 1, float q = 0, float size = 0)
     {
         GameObject obj = unityFire.addItem("Sphere", v);
+        unityFire.setItemScale(obj, size, size, size);
         phyObj po = obj.GetComponent<phyObj>();
         po.setm(m);
         po.setq(q);
--- Assets/init.cs
+++ Assets/init.cs
@@ -19,8 +19,8 @@ public class init : MonoBehaviour {
         Vector3 d2 = new Vector3(0, 1, 0);
         unityFire.addItemVelocity(o1, 1.2f, calu.normalVector(d1,d2));
 
-        creatObj.creatObject(new Vector3(3, 3, 3), 99999999999);
-        creatObj.creatObject(new Vector3(4, 4, 4), 99999999999);
+        creatObj.creatObject(new Vector3(3, 3, 3), 99999999999, 0, 1);
+        creatObj.creatObject(new Vector3(4, 4, 4), 99999999999, 0, 1);
     }
 	
 }
